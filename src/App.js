9bae6c4f97979fc866e3653bc0aa6350d4c656dc
kamkonphrom <kamkonphrom@gmail.com>
2021-08-31 19:30:21 +0700

    Add reducer and router

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './App.css'
 import { useState, useEffect} from 'react';
 import DataContext from "./data/DataContext";
 import ReportComponent from "./components/ReportComponent";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 
 function App() {
   const design = {color : "red", textAlign : "center", fontSize : '1.5rem'}
-  const initState = [
-    {id: 1, title: "ค่ารักษาพยาบาล",amount: -2000},
-    {id: 2, title: "เงินเดือน",amount: 50000},
-    {id: 3, title: "ค่าเดินทาง",amount: -500}
+  
+  const initData = [
+    {id: 1, title: "ค่าเช่าบ้าน", amount:-3000},
+    {id: 2, title: "เงินเดือน", amount:50000}
   ]
-  const [items, setItems] = useState(initState)
-  //const [items, setItems] = useState([])
-
+  const [items, setItems] = useState(initData)
   const [reportIncom, setReportIncome] = useState(0)
   const [reportExpense, setReportExpense] = useState(0)
   const onAddNewItem = (newItem) => {
@@ -28,21 +27,34 @@ function App() {
     const income = amounts.filter(element => element > 0).reduce((total, element) => total+=element,0)
     const expense = amounts.filter(element => element < 0).reduce((total, element) => total+=element,0)*-1
 
-    setReportIncome(income)
-    setReportExpense(expense)
+    setReportIncome(income.toFixed(2))
+    setReportExpense(expense.toFixed(2))
   },[items, reportIncom, reportExpense])
   return (
-    <DataContext.Provider value={
-      {
-        income : reportIncom,
-        expense : reportExpense
-      }
-    }>
-      <div className = "container">
+    <DataContext.Provider value={{income : reportIncom, expense : reportExpense}}>
+    <div className = "container">
       <h1 style = {design}>แอพบัญชีรายรับ - รายจ่าย</h1>
-      <ReportComponent/>
-      <FormComponent onAddItem = {onAddNewItem}/>
-      <Transaction items = {items}/>
+      <Router>
+      <div>
+        <ul className="horizontal-menu">
+          <li>
+            <Link to="/">ข้อมูลบัญชี</Link>
+          </li>
+          <li>
+          <Link to="/insert">บันทึกข้อมูล</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Router path="/" exact>
+            <ReportComponent/>
+          </Router>
+          <Router path="/insert">
+            <FormComponent onAddItem = {onAddNewItem}/>
+            <Transaction items = {items}/>
+          </Router>
+        </Switch>
+      </div>
+      </Router>
     </div>
     </DataContext.Provider>
   );
