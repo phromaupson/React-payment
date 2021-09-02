383d58e7199a1411d1ef60e47d4f14f8261f55dd
kamkonphrom <kamkonphrom@gmail.com>
2021-09-02 20:52:27 +0700

    deploy payment app to netlify

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,8 +5,11 @@ const Item = (props) => {
     const {title,amount} = props
     const status = amount < 0 ? "expense" :"income"
     const symbol = amount < 0 ? "-" :"+"
+    const formatNumber = (num) => {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
     return (
-        <li className = {status}>{title}  <span>{symbol}{Math.abs(amount)}</span>
+        <li className = {status}>{title}  <span>{symbol}{formatNumber(Math.abs(amount))}</span>
       </li>
     );
 }
